bbcc24e50cc46cbf5445ce735d5ab071375c2474
Daphaz <bonnet.damien@ymail.com>
2021-02-19 13:27:14 +0100

    feat: loading system with nprogress
    Use the Router event : "routeChangeStart","routeChangeComplete"
    "routeChangeError"

--- pages/_app.js
+++ pages/_app.js
@@ -3,12 +3,30 @@ import "bootstrap-css-only/css/bootstrap.min.css";
 import "mdbreact/dist/css/mdb.css";
 import "../styles/globals.css";
 import { AuthProvider } from "../auth/context";
+import Nprogress from "nprogress";
+import Router from "next/router";
+import Head from "next/head";
+
+Router.events.on("routeChangeStart", () => {
+	Nprogress.start();
+});
+Router.events.on("routeChangeComplete", () => {
+	Nprogress.done();
+});
+Router.events.on("routeChangeError", () => {
+	Nprogress.start();
+});
 
 function MyApp({ Component, pageProps }) {
 	return (
-		<AuthProvider>
-			<Component {...pageProps} />
-		</AuthProvider>
+		<>
+			<Head>
+				<link rel="stylesheet" type="text/css" href="/nprogress.css" />
+			</Head>
+			<AuthProvider>
+				<Component {...pageProps} />
+			</AuthProvider>
+		</>
 	);
 }
 
